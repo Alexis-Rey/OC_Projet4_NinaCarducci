57694098894371840700e7bbfc6f071c7c80e6b8
Alexis REY <alexis.rey159@laposte.net>
2025-06-02 10:47:40 +0200

    Analyse du JS pour compréhension

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,8 @@
+// On attends que tout les éléments du DOM soit chargés avant d'éxecuter la fonction
 $(document).ready(function() {
+    // On séléctionne l'élément correspondant donc ici la galerie de photo pour lui affecté le plugin mauGallery
     $('.gallery').mauGallery({
+        // On renseigne le nombre de colonne en options du plugin permettant ainsi de rendre cette dernière responsive ( xs : trés petit par exemple )
         columns: {
             xs: 1,
             sm: 2,
@@ -7,6 +10,8 @@ $(document).ready(function() {
             lg: 3,
             xl: 3
         },
+        // Autres options tel que lightBox pour afficher les images séléctionné en superposition agrandie, désignation de la lightbox avec un id pour la différencier 
+        // des autres instances lightbox, affichage ou non des tags (ici oui) et positionnement de ces derniers par rapport à la galerie ( ici en haut)
         lightBox: true,
         lightboxId: 'myAwesomeLightbox',
         showTags: true,
