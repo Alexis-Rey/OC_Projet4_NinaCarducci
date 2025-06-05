07ba10d55d2fa98397daa2e8f56c9e07083e5284
Alexis REY <alexis.rey159@laposte.net>
2025-06-05 20:05:39 +0200

    modif et ajout du début carouselle

---
+++ assets/carousel.js
@@ -0,0 +1,28 @@
+const imagesCarousel = document.querySelectorAll(".carousel__image");
+const boutonsMenu = document.querySelectorAll(".carousel__bouton");
+const boutonPrev = document.querySelector(".carousel__prev");
+const boutonNext = document.querySelector(".carousel__next");
+
+let currentIndex = 0;
+let interval = setInterval(nextImage,3000);
+
+function updateCarousel(index){
+    for ( let i = 0; i<imagesCarousel.length; i++){
+        imagesCarousel[i].setAttribute("aria-hidden", i !== index);
+        imagesCarousel[i].setAttribute("aria-current", i === index);
+    }
+    for ( let j = 0; j<boutonsMenu.length; j++){
+        boutonsMenu[j].classList.remove("carousel__bouton--actived");
+        boutonsMenu[j].setAttribute("aria-current", j === index);
+        let nextBouton = boutonsMenu[j].getAttribute("aria-current");
+        if (nextBouton === "true"){
+          boutonsMenu[j].classList.add("carousel__bouton--actived");  
+        }
+    }
+    currentIndex = index;
+};
+
+function nextImage(){
+    let nextIndex = (currentIndex + 1) % imagesCarousel.length;
+    updateCarousel(nextIndex);
+};
