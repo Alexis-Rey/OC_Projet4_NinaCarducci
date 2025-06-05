748ffe0709047c030782c663d7a73bfca119421b
Alexis REY <alexis.rey159@laposte.net>
2025-06-06 01:05:31 +0200

    Carousel presque parfait sauf retour finale

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,22 +1,24 @@
+const gallery = document.querySelector(".carousel__gallery");
 const imagesCarousel = document.querySelectorAll(".carousel__image");
 const boutonsMenu = document.querySelectorAll(".carousel__bouton");
 const boutonPrev = document.querySelector(".carousel__prev");
 const boutonNext = document.querySelector(".carousel__next");
 
 let currentIndex = 0;
-let interval = setInterval(nextImage,5000);
+let interval = setInterval(nextImage,3000);
 
 function updateCarousel(index){
     for ( let i = 0; i<imagesCarousel.length; i++){
         imagesCarousel[i].setAttribute("aria-hidden", i !== index);
         imagesCarousel[i].setAttribute("aria-current", i === index);
+        gallery.style.transform = "translateX(-" + (index * 100) + "%)";
     }
     for ( let j = 0; j<boutonsMenu.length; j++){
         boutonsMenu[j].classList.remove("carousel__bouton--actived");
         boutonsMenu[j].setAttribute("aria-current", j === index);
         let nextBouton = boutonsMenu[j].getAttribute("aria-current");
         if (nextBouton === "true"){
-          boutonsMenu[j].classList.add("carousel__bouton--actived");  
+          boutonsMenu[j].classList.add("carousel__bouton--actived"); 
         }
     }
     currentIndex = index;
@@ -36,7 +38,7 @@ for(let b = 0; b<boutonsMenu.length; b++){
     boutonsMenu[b].addEventListener("click", () =>{
         clearInterval(interval);
         updateCarousel(b);
-        interval = setInterval(nextImage,5000);
+        interval = setInterval(nextImage,3000);
     });
 };
 
